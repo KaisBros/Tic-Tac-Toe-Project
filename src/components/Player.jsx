a7d5b56a813a69c9fb4784b5d94ffe29a9c3160e
KaisBros <vt.dang000@gmail.com>
2024-02-23 00:33:49 +0700

    Add player name change functionality

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,32 +1,36 @@
 import { useState } from "react";
 
-export default function Player({ initialName, symbol, isActive }) {
-    const [ playerName, setPlayerName] = useState(initialName);
-    const [ isEditing, setIsEditing ] = useState(false);
+export default function Player({ initialName, symbol, isActive, onChangeName }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
 
-    function handleEditClick() {
-        setIsEditing(isEditing => !isEditing);
-    }
-
-    function handleChange(event) {
-        setPlayerName(event.target.value);
-    }
-
-    let editablePlayerName = <span className="player-name">{playerName}</span>;
-
+  function handleEditClick() {
+    setIsEditing((isEditing) => !isEditing);
+    
     if (isEditing) {
-        editablePlayerName = <input type="text" value={playerName} onChange={handleChange} required />
+        onChangeName(symbol, playerName);
     }
+  }
 
-    return (
-        <li className={isActive ? "active" : undefined}>
-        <span className="player">
-            {editablePlayerName}
-            <span className="player-symbol">{symbol}</span>
-        </span>
-        <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
-    </li>
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
 
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
 
+  if (isEditing) {
+    editablePlayerName = (
+      <input type="text" value={playerName} onChange={handleChange} required />
     );
+  }
+
+  return (
+    <li className={isActive ? "active" : undefined}>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
 }
